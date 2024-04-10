221c7da93ab369ff79dedfbc3b70aa2566e5788c
Aleksander Fasting <aleksasf@stud.ntnu.no>
2024-04-10 03:59:39 +0200

    2way connect

--- src/main/java/project/InputHandler.java
+++ src/main/java/project/InputHandler.java
@@ -4,9 +4,17 @@ import java.net.InetAddress;
 
 import javafx.scene.control.ListView;
 
-public class InputHandler {
+public class InputHandler{
     private static Sender sender = new Sender(12180);
     private static Receiver receiver = new Receiver(12080);
+    private static InetAddress address;
+    static {
+        try {
+            address = InetAddress.getByName("192.168.0.106");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void handleInput(ListView<String> field, String input) {
         if (input.charAt(0) == ':') {
@@ -20,6 +28,14 @@ public class InputHandler {
         field.getItems().add("You: " + message);
     }
 
+    public static void sendMessage(String message) {
+        try {
+            sender.sendMessage(message, address, 12080);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void handleCommand(ListView<String> field, String command) {
         if (command.equals(":help")) {
             field.getItems().add("    Commands:");
@@ -31,20 +47,22 @@ public class InputHandler {
             field.getItems().clear();
             return;
         }
-        if (command.equals(":send")) {  
-            try {
-                String message = "Hello";
-                InetAddress address = InetAddress.getByName("192.168.0.106");
-                sender.sendMessage(message, address, 12080);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if (command.equals(":send")) {
+            sendMessage("Hello, World!");
             return;
         }
         if (command.equals(":receive")) {
             String message = receiver.receiveMessage();
             field.getItems().add("Received: " + message);
             return;
+        }
+        if (command.equals(":connect")) {
+            String IP = command.substring(9);
+            try {
+                address = InetAddress.getByName(IP);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         field.getItems().add("Unknown command: " + command);
     }
